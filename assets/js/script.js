1f6131cb78dbaabee35eca38c70140a27c40b8b6
Jorge Cortes <chochelius@gmail.com>
2021-11-27 03:23:48 -0300

    segundo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 console.log("Conectado!")
+
+let miIife = (() => {
+	let funcionPrivada = (url, id) => {
+		id.setAttribute('src', url);
+	};
+	return {
+		funcionPublica: (url, id) => funcionPrivada(url, id),
+	};
+})();
+
+
 class Multimedia {
     constructor(url) {
         this.url = url;        
@@ -12,21 +23,35 @@ class Multimedia {
 }
 
 class Reproductor extends Multimedia {
-    constructor(url, id) {
-        super(url);
-        this.id = id;
-    };
-    get getId() {
-        return this.id
-    };
-    
-    setInicio(segundos) {
-        return segundos
-    };
-    playMultimedia() {
-
-    }
-
+	constructor(url, id) {
+		super(url);
+		this.id = id;
+	}
+	playMultimedia() {
+		miIife.funcionPublica(this.getUrl, this.id);
+	}
+	setInicio(time) {
+		this.id.setAttribute('src', `${this.getUrl}?start=${time}`);
+	}
 }
 
-//aún me falta montón pero está tomando forma
+const vistaMusic = new Reproductor(
+	'https://www.youtube.com/embed/aRLR8-oNPZM',
+	musica
+);
+vistaMusic.playMultimedia();
+vistaMusic.setInicio(336);
+
+const vistaPeli = new Reproductor(
+	'https://www.youtube.com/embed/c_3NMtxeyfk',
+	peliculas
+);
+vistaPeli.playMultimedia();
+vistaPeli.setInicio(30);
+
+const vistaSerie = new Reproductor(
+	'https://www.youtube.com/embed/tcrNsIaQkb4',
+	series
+);
+vistaSerie.playMultimedia();
+vistaSerie.setInicio(50);
